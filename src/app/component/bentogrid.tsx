f6a3689d10720dac4dcddb35c5e1139284dd1df1
jinu2004 <jinujs53@gmail.com>
2025-03-25 16:41:19 +0000

    added herosection contents

--- src/app/component/bentogrid.tsx
+++ src/app/component/bentogrid.tsx
@@ -1,5 +1,4 @@
-import { BuildingOffice2Icon, BuildingOfficeIcon } from '@heroicons/react/20/solid';
-import { AcademicCapIcon, BriefcaseIcon, TrophyIcon } from '@heroicons/react/24/outline';
+import { AcademicCapIcon, BuildingOfficeIcon, TrophyIcon  } from '@heroicons/react/24/outline';
 import "./css/style.css"
 
 export default function BentoGridHeroSection() {
@@ -22,10 +21,10 @@ export default function BentoGridHeroSection() {
                 <div className="div2 w-44">
                     <div className='rounded-lg bg-[#4d53e6] flex-col flex justify-center'>
                         <div className="flex px-2 py-2">
-                            <div className="flex-auto"><BuildingOfficeIcon className='size-6' /></div>
-                            <div className="flex-auto ">Company</div>
+                            <div className="flex-auto"><AcademicCapIcon className='size-6' /></div>
+                            <div className="flex-auto ">Graduation</div>
                         </div>
-                        <p className='text-[9px] px-2 text-pretty py-2'>
+                        <p className='text-[11px] px-2 text-pretty py-2'>
                             Strong company relations in a college open doors to career opportunities, internships, and real-world industry exposure.
                         </p>
                     </div>
@@ -33,11 +32,11 @@ export default function BentoGridHeroSection() {
                 <div className="div3 w-44">
                     <div className='rounded-lg bg-[#4d53e6] flex-col flex justify-center'>
                         <div className="flex px-2 py-2">
-                            <div className="flex-auto"><BuildingOfficeIcon className='size-6' /></div>
-                            <div className="flex-auto ">Company</div>
+                            <div className="flex-auto"><TrophyIcon className='size-6' /></div>
+                            <div className="flex-auto ">Achivement</div>
                         </div>
-                        <p className='text-[9px] px-2 text-pretty py-2'>
-                            Strong company relations in a college open doors to career opportunities, internships, and real-world industry exposure.
+                        <p className='text-[11px] px-2 text-pretty py-2'>
+                        The achievements we earn in college shape our future, proving that dedication and passion lead to success.
                         </p>
                     </div>
                 </div>
