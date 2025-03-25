aaaea6425450799960755c6ddc2035709cd00c44
jinu2004 <jinujs53@gmail.com>
2025-03-25 16:26:10 +0000

    updated hero section

--- src/app/component/bentogrid.tsx
+++ src/app/component/bentogrid.tsx
@@ -1,3 +1,49 @@
-import { AcademicCapIcon , BriefcaseIcon, TrophyIcon} from '@heroicons/react/24/outline';
+import { BuildingOffice2Icon, BuildingOfficeIcon } from '@heroicons/react/20/solid';
+import { AcademicCapIcon, BriefcaseIcon, TrophyIcon } from '@heroicons/react/24/outline';
+import "./css/style.css"
+
+export default function BentoGridHeroSection() {
+    return (
+        <div className='lg:max-w-lg overflow-visible relative '>
+            <img src='/bg/girl_cover.png' className='absolute left-32 w-auto h-auto'></img>
+
+            <div className="parent relative m-10">
+                <div className="div1 w-44">
+                    <div className='rounded-lg bg-[#4d53e6] flex-col flex justify-center'>
+                        <div className="flex px-2 py-2">
+                            <div className="flex-auto"><BuildingOfficeIcon className='size-6' /></div>
+                            <div className="flex-auto ">Company</div>
+                        </div>
+                        <p className='text-[9px] px-2 text-pretty py-2'>
+                            Strong company relations in a college open doors to career opportunities, internships, and real-world industry exposure.
+                        </p>
+                    </div>
+                </div>
+                <div className="div2 w-44">
+                    <div className='rounded-lg bg-[#4d53e6] flex-col flex justify-center'>
+                        <div className="flex px-2 py-2">
+                            <div className="flex-auto"><BuildingOfficeIcon className='size-6' /></div>
+                            <div className="flex-auto ">Company</div>
+                        </div>
+                        <p className='text-[9px] px-2 text-pretty py-2'>
+                            Strong company relations in a college open doors to career opportunities, internships, and real-world industry exposure.
+                        </p>
+                    </div>
+                </div>
+                <div className="div3 w-44">
+                    <div className='rounded-lg bg-[#4d53e6] flex-col flex justify-center'>
+                        <div className="flex px-2 py-2">
+                            <div className="flex-auto"><BuildingOfficeIcon className='size-6' /></div>
+                            <div className="flex-auto ">Company</div>
+                        </div>
+                        <p className='text-[9px] px-2 text-pretty py-2'>
+                            Strong company relations in a college open doors to career opportunities, internships, and real-world industry exposure.
+                        </p>
+                    </div>
+                </div>
+            </div>
 
 
+        </div>
+    )
+}
--- src/app/homepage/HeroSection.tsx
+++ src/app/homepage/HeroSection.tsx
@@ -1,4 +1,5 @@
 import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import BentoGridHeroSection from '../component/bentogrid'
 
 
 const features = [
@@ -22,34 +23,29 @@ const features = [
 
 export default function HeroSection() {
     return (
-        <div className="overflow-hidden bg-white py-24 sm:py-32">
+        <div className="overflow-hidden py-24 sm:py-32 select-none">
+
+            <img src='/bg/IMG-20250318-WA0000.jpg' className='opacity-15 -z-50 w-full object-cover object-center h-full absolute top-3 left-0'></img>
+
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-                    <div className="lg:pt-4 lg:pr-8">
+                    <div className="lg:pt-2 lg:pr-8">
                         <div className="lg:max-w-lg">
-                            <div className="absolute top-[359px] left-[56px] w-[795px] h-[595px]">
-                                <div className="text-[#c8c6c6] font-medium capitalize text-2xl leading-[1.2] absolute top-[359px] left-[12px] w-[783px]">
+                            <div className="relative w-max h-screen flex flex-col gap-7">
+                                <h1 className="font-black capitalize text-6xl leading-[1.2] max-w-2xl text-start">
+                                    The <span className='text-[#4c52e6]'> best place</span>  to chase your <br /> <span className='text-[#4c52e6]' >dreams</span>
+                                </h1>
+                                <p className="text-[#c8c6c6] font-medium capitalize text-base leading-[1.2] relative max-w-xl">
                                     The Best Place to Shape Your Future Ace College of Engineering is a center of excellence, fostering innovation and hands-on learning. Our dynamic campus and expert faculty prepare students for a successful caree
-                                </div>
-                                <div className="font-black capitalize text-[80px] leading-[1.2] absolute top-[0px] left-[12px] w-[728px]">
-                                    The best place to chase your dreams
-                                </div>
-                                <div className="absolute top-[528px] left-[0px] w-[259px] h-[67px]">
-                                    <div className="absolute top-[0px] left-[0px] w-[259px] h-[67px] bg-[#4c52e6] rounded-[20px]"></div>
-                                    <div className="text-white capitalize text-3xl leading-[1.2] absolute top-[15px] left-[43px] w-[172px] h-[37px]">
-                                        Apply Now
-                                    </div>
-                                </div>
+                                </p>
+                                <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 w-fit text-lg  text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Apply Now</a>
                             </div>
 
 
                         </div>
                     </div>
 
-                    <img alt="Product screenshot"
-                        src="/logos/Group.png"
-                        className="w-1/5 max-w-none  sm:w-1/5 md:-ml-4 lg:-ml-0"
-                        />
+                    <BentoGridHeroSection/>
 
 
                 </div>
