ee776fc5c405660403416b91c2c958b2b7152522
jinuj <jinujs53@gmail.com>
2025-03-26 14:28:54 +0530

    updated  and added some gifs and added WhyAceSection(second)section

--- src/app/component/Bentogrid2.tsx
+++ src/app/component/Bentogrid2.tsx
@@ -6,59 +6,59 @@ export default function BentoGrid2() {
                 aria-hidden="true"
                 className="pointer-events-none lg:absolute lg:inset-y-0"
             >
-                <div className="absolute transform sm:top-0 sm:left-1/2 sm:translate-x-8 lg:top-1/2 lg:left-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                <div className="absolute transform sm:top-0 sm:left-1/2 sm:translate-x-8  lg:left-1/2  lg:translate-x-8">
                     <div className="flex items-center space-x-6 lg:space-x-8">
                         <div className="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                            <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
+                            <div className="w-56 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
                                 <img
                                     alt=""
-                                    src="https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
+                                    src="/gridgifs/9n7sc2-ezgif.com-crop (1).gif.gif"
                                     className="size-full object-cover"
                                 />
                             </div>
-                            <div className="h-64 w-44 overflow-hidden rounded-lg">
+                            <div className="w-56 overflow-hidden rounded-lg">
                                 <img
                                     alt=""
-                                    src="https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-03-hero-image-tile-02.jpg"
-                                    className="size-full object-cover"
-                                />
-                            </div>
-                        </div>
-                        <div className="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                            <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                <img
-                                    alt=""
-                                    src="https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-03-hero-image-tile-03.jpg"
-                                    className="size-full object-cover"
-                                />
-                            </div>
-                            <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                <img
-                                    alt=""
-                                    src="https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-03-hero-image-tile-04.jpg"
-                                    className="size-full object-cover"
-                                />
-                            </div>
-                            <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                <img
-                                    alt=""
-                                    src="https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-03-hero-image-tile-05.jpg"
+                                    src="/gridgifs/gif1-ezgif.com-crop.gif.gif"
                                     className="size-full object-cover"
                                 />
                             </div>
                         </div>
                         <div className="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                            <div className="h-64 w-44 overflow-hidden rounded-lg">
+                            <div className="w-56 overflow-hidden rounded-lg">
                                 <img
                                     alt=""
-                                    src="https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-03-hero-image-tile-06.jpg"
+                                    src="/gridgifs/labAiro-ezgif.com-crop.gif.gif"
                                     className="size-full object-cover"
                                 />
                             </div>
-                            <div className="h-64 w-44 overflow-hidden rounded-lg">
+                            <div className="w-56 overflow-hidden rounded-lg">
                                 <img
                                     alt=""
-                                    src="https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-03-hero-image-tile-07.jpg"
+                                    src="/gridgifs/gif3-ezgif.com-crop.gif.gif"
+                                    className="size-full object-cover"
+                                />
+                            </div>
+                            <div className="w-56 overflow-hidden rounded-lg">
+                                <img
+                                    alt=""
+                                    src="/gridgifs/gif4-ezgif.com-optimize.gif.gif"
+                                    className="size-full object-cover"
+                                />
+                            </div>
+                        </div>
+                        <div className="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                            <div className="w-56 overflow-hidden rounded-lg">
+                                <img
+                                    alt=""
+                                    src="/gridgifs/gif5-ezgif.com-crop.gif.gif"
+                                    className="size-full object-cover"
+                                />
+                            </div>
+                            <div className="w-56 overflow-hidden rounded-lg">
+                                <img
+                                    alt=""
+                                    src="/gridgifs/gif8-ezgif.com-crop.gif.gif"
                                     className="size-full object-cover"
                                 />
                             </div>
