2a3ccef4bdc14ad9acd55c1dda558e269ac1c008
aswanth-codes <aswanthbsujan.dev@gmail.com>
2025-03-27 16:07:35 +0530

    Added Principal

--- src/app/Principal/Principal.tsx
+++ src/app/Principal/Principal.tsx
@@ -1,15 +1,32 @@
 export default function Principal(){
     return(
-        <div className="absolute top-[7644px] left-[337px] w-[1920px] h-[1080px] bg-white">
-            <div className="absolute top-[335px] left-[-1005px] w-[2208px] h-[446px] blur-[100px]"></div>
-            <div className="absolute top-[335px] left-[-1005px] w-[2208px] h-[446px] blur-[100px]"></div>
-            <div className="absolute top-[-777px] left-[658px] w-[2208px] h-[446px] blur-[100px]"></div>
-            <img src="https://picsum.photos/808/537" className="absolute top-[312px] left-[90px] w-[808px] h-[537px] rounded-[29px]"/>
-            <div className="font-black uppercase text-[78px] leading-[1.2] absolute top-[-18px] left-[92px] w-[446px] h-[302px]">
-                meet our principal
+        <div
+            className="relative w-full h-auto"
+            style={{
+                background: "white",
+                backgroundImage: "url('/backgroundSvg/svg_bg_princ_bottom.png'), url('/backgroundSvg/svg_bg_princ_top.png')",
+                backgroundSize: "50%, 100%",
+                backgroundRepeat: "no-repeat, no-repeat",
+                backgroundPosition: "bottom left, top right",
+            }}
+            // style={{
+            //     background: "linear-gradient(28deg, #4145cc 10%,white 30%, white 70%, #4145cc 90%)", // Create gradient with rotation and colors
+            // }}
+        >
+
+            <div className="font-black uppercase text-6xl leading-[1.2] pt-16 pl-20 w-[446px]">
+                MEET OUR <p className="text-blue-700">PRINCIPAL</p>
             </div>
-            <div className="text-[#040404] font-medium capitalize text-[28px] leading-[1.2] absolute top-[277px] left-[1005px] w-[818px]">
-                Meet Our Principal Dr. Farrukh Sayeed is a visionary academic leader dedicated to nurturing student excellence through a perfect blend of academics and extracurricular activities. Under his dynamic leadership, ACE College of Engineering fosters a culture of holistic development, empowering students with leadership, teamwork, and communication skills. With an unwavering commitment to innovation and excellence, Dr. Sayeed ensures that students have access to the right platforms and opportunities to unlock their full potential. His passion for education and mentorship continues to inspire the next generation of engineers and leaders. Dr. Farrukh Sayeed
+            <div className="flex flex-col md:flex-col lg:flex-row justify-around items-center flex-wrap gap-2 p-4 h-auto">
+                <img src="principalphoto.jpg" className=" md:w-[90%] lg:w-[42%]  rounded-[29px]"/>
+                <div className="pb-8 text-[#040404] font-medium  font-inter capitalize text-2xl leading-[1.2] lg:w-[42%]">
+                    Meet Our Principal<br/><br/> Dr. Farrukh Sayeed is a visionary academic leader dedicated to nurturing student excellence through a 
+                    perfect blend of academics and extracurricular activities. Under his dynamic leadership, ACE College of Engineering fosters 
+                    a culture of holistic development, empowering students with leadership, teamwork, and communication skills. <br/><br/>With an unwavering 
+                    commitment to innovation and excellence, Dr. Sayeed ensures that students have access to the right platforms and opportunities 
+                    to unlock their full potential. His passion for education and mentorship continues to inspire the next generation of engineers 
+                    and leaders.
+                </div>
             </div>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import HeroSection from "./homepage/HeroSection";
 import WhyAceSection from "./homepage/WhyAceSection";
 import Industries from "./Industries/Industries";
 import CourcesList from "./homepage/CourceSection";
+import Principal from "./Principal/Principal";
 export default function Home() {
   return (<div className="select-none">
     <NavBar />
     <HeroSection />
     <WhyAceSection />
     <CourcesList/>
+    <Principal/>
     <Industries/>
     <Footer />
 
