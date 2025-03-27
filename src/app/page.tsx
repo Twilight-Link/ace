08f3811b6e54fb3f8a3a084a0405cf61522b93ba
aswanth-codes <aswanthbsujan.dev@gmail.com>
2025-03-27 22:17:55 +0530

    moved principal and industries to homepage folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import NavBar from "./component/NavBar";
 import Footer from "./component/Footer";
 import HeroSection from "./homepage/HeroSection";
 import WhyAceSection from "./homepage/WhyAceSection";
-import Industries from "./Industries/Industries";
+import Industries from "./homepage/Industries";
 import CourcesList from "./homepage/CourceSection";
-import Principal from "./Principal/Principal";
+import Principal from "./homepage/Principal";
 import HallOfFame from "./halloffame/halloffame";
 export default function Home() {
   return (<div className="select-none">
