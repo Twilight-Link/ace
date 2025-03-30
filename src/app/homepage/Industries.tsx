6830c94f5f5521a93472a6a52b7bf9004cab292d
jinuj <jinujs53@gmail.com>
2025-03-30 20:26:59 +0530

    added animations

--- src/app/homepage/Industries.tsx
+++ src/app/homepage/Industries.tsx
@@ -1,6 +1,32 @@
+'use client'
+import { useInView, useAnimation, motion } from "framer-motion";
+import { useRef, useEffect } from "react";
+
 export default function Industries() {
+    const scrollRef = useRef(null);
+    const isInview = useInView(scrollRef, { once: true });
+    const mainDivController = useAnimation();
+    const divVarients = {
+        hidden: { opacity: 0.5, x: 75 },
+        visible: {
+            opacity: 1,
+            x: 0,
+            transition: { duration: 1, ease: "linear" },
+        },
+    };
+
+    useEffect(() => {
+        if (isInview) {
+            mainDivController.start("visible");
+        }
+    }, [isInview]);
+
     return (
-        <div
+        <motion.div
+            ref={scrollRef}
+            variants={divVarients}
+            initial="hidden"
+            animate={mainDivController}
             className="relative w-full max-w-[100vw] bg-gradient-to-r flexflex-col items-center pt-[12dvh] pb-[18dvh]"
             style={{
                 background: "black",
@@ -15,12 +41,27 @@ export default function Industries() {
             </div>
             <div className=" left-0 w-full flex justify-between items-center px-[9.15vw]">
                 {/* <embed src="logos/industriesLogo/TCS-crop.svg" type="image/svg+xml" className="w-[12.52vw] h-[12.5vw]" /> */}
-                <img src="logos/industriesLogo\TCS-crop.png" className="w-[12.02vw] h-[12.02vw]" />
-                <img src="logos/industriesLogo\icons8-apple-250.png" className="w-[11.02vw] h-[11.02vw]" />
-                <img src="logos/industriesLogo\icons8-windows-250.png" className="w-[11.02vw] h-[11.02vw]" />
-                <img src="logos/industriesLogo\google-icon.svg" className="w-[9.02vw] h-[11.02vw]" />
-                <img src="logos/industriesLogo\mahindra_logo.svg" className="w-[13.02vw] h-[12.02vw]" />
+                <motion.img
+                    whileTap={{ scale: 1.3 }}
+                    whileHover={{ scale: 1.3 }}
+                    transition={{ type: "spring", stiffness: 300 }} src="logos/industriesLogo\TCS-crop.png" className="w-[12.02vw] h-[12.02vw]" />
+                <motion.img
+                    whileTap={{ scale: 1.3 }}
+                    whileHover={{ scale: 1.3 }}
+                    transition={{ type: "spring", stiffness: 300 }} src="logos/industriesLogo\icons8-apple-250.png" className="w-[11.02vw] h-[11.02vw]" />
+                <motion.img
+                    whileTap={{ scale: 1.3 }}
+                    whileHover={{ scale: 1.3 }}
+                    transition={{ type: "spring", stiffness: 300 }} src="logos/industriesLogo\icons8-windows-250.png" className="w-[11.02vw] h-[11.02vw]" />
+                <motion.img
+                    whileTap={{ scale: 1.3 }}
+                    whileHover={{ scale: 1.3 }}
+                    transition={{ type: "spring", stiffness: 300 }} src="logos/industriesLogo\google-icon.svg" className="w-[9.02vw] h-[11.02vw]" />
+                <motion.img
+                    whileTap={{ scale: 1.3 }}
+                    whileHover={{ scale: 1.3 }}
+                    transition={{ type: "spring", stiffness: 300 }} src="logos/industriesLogo\mahindra_logo.svg" className="w-[13.02vw] h-[12.02vw]" />
             </div>
-        </div>
+        </motion.div>
     );
 }
--- src/app/homepage/Principal.tsx
+++ src/app/homepage/Principal.tsx
@@ -1,6 +1,36 @@
-export default function Principal(){
-    return(
-        <div
+'use client'
+import { useInView, useAnimation, motion } from "framer-motion";
+import { useRef, useEffect } from "react";
+
+export default function Principal() {
+
+    const scrollRef = useRef(null);
+    const isInview = useInView(scrollRef, { once: true });
+    const mainDivController = useAnimation();
+    const divVarients = {
+        hidden: { opacity: 0.5, y: 75 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1, ease: "linear" },
+        },
+    };
+
+    useEffect(() => {
+        if (isInview) {
+            mainDivController.start("visible");
+        }
+    }, [isInview]);
+
+
+
+
+    return (
+        <motion.div
+            ref={scrollRef}
+            variants={divVarients}
+            initial="hidden"
+            animate={mainDivController}
             className="relative w-full h-auto"
             style={{
                 background: "white",
@@ -9,25 +39,25 @@ export default function Principal(){
                 backgroundRepeat: "no-repeat, no-repeat",
                 backgroundPosition: "bottom left, top right",
             }}
-            // style={{
-            //     background: "linear-gradient(28deg, #4145cc 10%,white 30%, white 70%, #4145cc 90%)", // Create gradient with rotation and colors
-            // }}
+        // style={{
+        //     background: "linear-gradient(28deg, #4145cc 10%,white 30%, white 70%, #4145cc 90%)", // Create gradient with rotation and colors
+        // }}
         >
 
             <div className="font-black uppercase text-6xl text-black leading-[1.2] pt-16 pl-20 w-[446px]">
                 MEET OUR <p className="text-blue-700">PRINCIPAL</p>
             </div>
             <div className="flex flex-col md:flex-col lg:flex-row justify-around items-center flex-wrap gap-2 p-4 h-auto">
-                <img src="principalphoto.jpg" className=" md:w-[90%] lg:w-[42%]  rounded-[29px]"/>
+                <img src="principalphoto.jpg" className=" md:w-[90%] lg:w-[42%]  rounded-[29px]" />
                 <div className="pb-8 text-black font-normal  font-inter capitalize text-xl leading-[1.2] lg:w-[42%]">
-                    Meet Our Principal<br/><br/> Dr. Farrukh Sayeed is a visionary academic leader dedicated to nurturing student excellence through a 
-                    perfect blend of academics and extracurricular activities. Under his dynamic leadership, ACE College of Engineering fosters 
-                    a culture of holistic development, empowering students with leadership, teamwork, and communication skills. <br/><br/>With an unwavering 
-                    commitment to innovation and excellence, Dr. Sayeed ensures that students have access to the right platforms and opportunities 
-                    to unlock their full potential. His passion for education and mentorship continues to inspire the next generation of engineers 
+                    Meet Our Principal<br /><br /> Dr. Farrukh Sayeed is a visionary academic leader dedicated to nurturing student excellence through a
+                    perfect blend of academics and extracurricular activities. Under his dynamic leadership, ACE College of Engineering fosters
+                    a culture of holistic development, empowering students with leadership, teamwork, and communication skills. <br /><br />With an unwavering
+                    commitment to innovation and excellence, Dr. Sayeed ensures that students have access to the right platforms and opportunities
+                    to unlock their full potential. His passion for education and mentorship continues to inspire the next generation of engineers
                     and leaders.
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
